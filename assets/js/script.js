59d144e97a49d9ec4d2a02dc162f70fb9b8230ac
Martin Castaneda <martincastaneda@Martins-MacBook-Pro.local>
2023-11-13 19:04:33 -0800

    js-file-updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+/* Base element needed for the accordion to work. */
+var baseAccordion = $('#accordion');
+
+
+/* Need function to generate accordions for each set of input given. */
+function generateAccordion(resultCategory, resultOutput){
+    var headline = $('<h3>');
+    var descriptionText = $('<div>');
+
+    headline.text(resultCategory);
+    descriptionText.text(resultOutput);
+
+    baseAccordion.append(headline);
+    baseAccordion.append(descriptionText);
+}
+
 function sendApiRequest() {
     var userInput = document.getElementById("input").value
     console.log(userInput)
