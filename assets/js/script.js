19eff05ff6f14776ae1d7b017e73f42f92e6ec8b
Martin Castaneda <martincastaneda@Martins-MacBook-Pro.local>
2023-11-13 18:47:58 -0800

    js-update

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+function sendApiRequest() {
+    var userInput = document.getElementById("input").value
+    console.log(userInput)
+    var giphyApiKey = "3PETcBI1sQkizCJik9gKuWkHTt3Xojp0"
+    var giphyApiUrl = 'https://api.giphy.com/v1/gifs/categories?q=' + userInput + '&rating=g&api_key=' + giphyApiKey
+
+    fetch(giphyApiUrl).then(function (response) {
+        return response.json()
+    })
+        .then(function (data) {
+            console.log(json)
+            console.log(json.data[0].images.url)
+            var imgPath = json.data[0].images.fixed_height.url
+            var img = document.createElement("img")
+            img.setAttribute("src:", imgPath)
+            document.body.appendChild(img)
+
+        })
+
+    // fetch(giphyApiUrl, {
+    //     method: 'GET',
+    // })
+    // .then(function (response){
+    //     return response.json();
+    // })
+    // .then(function (data){
+    //     console.log(data);
+    // })
+
+    // function show(data) {
+    //     let tab = 
+    //         `<tr>
+    //           <th>Celebrities</th>
+    //           <th>Movies</th>
+    //           <th>Cartoons_and_Comics</th>
+    //          </tr>`;
+       
+    //     // Loop to access all rows 
+    //     for (let r of data.list) {
+    //         tab += `<tr> 
+    //     <td>${r.Celebrities} </td>
+    //     <td>${r.Movies}</td>
+    //     <td>${r.Cartoons_and_Comics}</td>          
+    // </tr>`;
+    //     }
+    //     // Setting innerHTML as tab variable
+    //     document.getElementById("Gifs").innerHTML = tab;
+    // }
+    
+}
