625c14b2777c71dfe35bce5839e02b18a0260023
Martin Castaneda <martincastaneda75@yahoo.com>
2023-11-20 20:08:01 -0800

    updated-files

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,41 +8,32 @@ var storage = [];
 
 
 
-
-
-
-
-
-
-
 searchBtn.addEventListener('click', checkInput);
 
 /* Kindly assisted by Prof. Farish Kashefinejad */
-function checkInput(){
+function checkInput() {
   var userEntry = searchBar.value.trim();
 
   getFavorite;
-  
 
   giphySearchTerms = userEntry;
   sendGiphyApiRequest();
 
-
   var string = "captain america pizza";
   var words = userEntry.split(' ');
   var combineWord = '';
-  for(var x = 0; x < words.length; x++) {  
+  for (var x = 0; x < words.length; x++) {
     var upperWord = words[x][0].toUpperCase() + words[x].substr(1);
     var correctWord = upperWord[0].toUpperCase() + upperWord[0].substr(1);
-  
+
 
     combineWord += upperWord + ' ';
-}
+  }
   wikiSearchTerms = combineWord;
   getWikiApi();
 }
 
-function setFavorite (){
+function setFavorite() {
   var tempFavArray = [];
   tempFavArray[0] = wikiSearchTerms;
   tempFavArray[1] = giphySearchTerms;
@@ -52,52 +43,41 @@ function setFavorite (){
 
 }
 
-function getFavorite(){
+function getFavorite() {
 
   storage = JSON.parse(localStorage.getItem('favorites'));
   var favorite1 = document.getElementById('favorite');
   var favorite2 = document.getElementById('button2');
 
-  if(storage != null){
+  if (storage != null) {
     favorite1.setAttribute('text', storage[0][0]);
     favorite1.setAttribute('data-wiki', storage[0][0]);
     favorite1.setAttribute('data-giphy', storage[0][1]);
 
-    if(storage[1] != null){
+    if (storage[1] != null) {
       favorite2.setAttribute('text', storage[1][0]);
       favorite1.setAttribute('data-wiki', storage[1][0]);
       favorite1.setAttribute('data-giphy', storage[1][1]);
     }
   }
-  
+
 
 }
 
-/* function invokeFavorite(){
-  
+function invokeFavorite() {
+
   // this.
 }
 
-
-
-
 function sendGiphyApiRequest() {
-  //event.preventDefault();
-  // var giphyText = document.getElementById("giphyInput");
-  // var giphySearchBoxText = giphyText.value.trim();
-  //var giphySearchBoxText = searchBar.value.trim();
   var giphyApiKey = "3PETcBI1sQkizCJik9gKuWkHTt3Xojp0";
- // var giphyApiUrl = 'https://api.giphy.com/v1/gifs/search?q=' + giphySearchBoxText + '&rating=g&api_key=' + giphyApiKey + '&limit=15';
-
   var giphyApiUrl = 'https://api.giphy.com/v1/gifs/search?q=' + giphySearchTerms + '&rating=g&api_key=' + giphyApiKey + '&limit=15';
   var gifTest = document.getElementById('giphySection');
-    if(gifTest.hasChildNodes){
-      while (gifTest.firstChild) {
-        gifTest.removeChild(gifTest.firstChild);
-      }
+  if (gifTest.hasChildNodes) {
+    while (gifTest.firstChild) {
+      gifTest.removeChild(gifTest.firstChild);
     }
-
-
+  }
   fetch(giphyApiUrl)
     .then(function (response) {
       return response.json()
@@ -118,15 +98,8 @@ function sendGiphyApiRequest() {
 }
 
 
-
-
-
-
-//var searchItem = searchBar.value();
-// document.getElementbyID('searchbar').value()
-
 function getWikiApi() {
-  
+
   var requestUrl = 'https://en.wikipedia.org/w/api.php?action=query&prop=extracts&titles=' + wikiSearchTerms + '&format=json&explaintext=true&exsectionformat=plain&origin=*';
   fetch(requestUrl)
     .then(function (response) {
@@ -144,5 +117,3 @@ function getWikiApi() {
 
     });
 }
-
-//searchBtn.addEventListener('click', getWikiApi);
